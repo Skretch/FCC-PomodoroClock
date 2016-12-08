cadeeababb47d529a7bab7efd004a6a27068b779
Anders Sortedal <sortedal3@gmail.com>
2016-12-08 22:19:37 +0100

    Stopped the clock from repeating after the break timer finished

--- main.js
+++ main.js
@@ -93,7 +93,6 @@ var timerObj = {
                         loop:false
                     });
                     sound.play();
-                    timerObj.startTimer(curMainTimerVal);
                 }
             }
         }else{
